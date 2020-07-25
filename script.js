27fe3a7199f0e9d459ba38c27f50aa6cb8e83e85
Md Mijanur Rahaman <mijanur696@gmail.com>
2020-07-25 09:56:50 +0530

    First commit
    Day one coding

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('login').addEventListener("click", function myfun(){
+     document.getElementById("mylogin").style.display="none";
+     document.getElementById("depositdiv").style.display="";
+     
+})
