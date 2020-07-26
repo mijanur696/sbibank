2dc3241ab663ee0e5bd9293254ae6d45f1b9fd86
Md Mijanur Rahaman <mijanur696@gmail.com>
2020-07-26 16:58:50 +0530

    Updated from PC
    Date:26.07.2020
    Time:4:58 PM

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ document.getElementById('login').addEventListener("click", function myfun(){
      document.getElementById("depositdiv").style.display="";
      
 })
+
+
+
+document.getElementById("deposit_button").addEventListener("click",function deposit(){
+     var deposit_value=document.getElementById("deposit_input").value;
+     document.getElementById("initial-deposit").innerHTML=deposit_value;
+});
+
+document.getElementById("withdraw_button").addEventListener("click",function withdraw(){
+     var withdraw_value=document.getElementById("withdraw_input").value;
+     document.getElementById("initial-withdraw").innerHTML=withdraw_value;
+});
